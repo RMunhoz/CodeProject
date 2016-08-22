ca7035c62ba93e2fcdf3e832374cbaf37fd464a4
RMunhoz <rogerio_munhoz@hotmail.com.br>
2016-08-22 08:28:15 -0300

    Trabalhando com Models - Completo

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,27 +5,39 @@ namespace CodeProject\Http\Controllers;
 use CodeProject\Client;
 use Illuminate\Http\Request;
 use CodeProject\Http\Requests;
+use PhpSpec\Exception\Exception;
 
 
 class ClientController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return Client::all();
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         Client::create($request->all());
+        return response()->json(['Client não encontrado']);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         return Client::find($id);
     }
 
-    public function destroy($id){
-        Client::find($id)->delete();
+    public function destroy($id)
+    {
+        if(Client::find($id)->delete())
+        {
+            return response()->json('Client deletado com sucesso');
+        }
+        return response()->json(['Client não encontrado']);
     }
 
-    public function update($id, Request $request){
+    public function update($id, Request $request)
+    {
         Client::find($id)->update($request->all());
+        return Client::find($id);
     }
 }
